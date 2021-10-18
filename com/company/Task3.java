7838cce6f6880068055efd5c446b5597ba3233fb
Tanitum <komlev.ra@phystech.edu>
2021-10-18 21:26:19 +0300

    Постепенно делаю 3 задание

---
+++ com/company/Task3.java
@@ -0,0 +1,27 @@
+package com.company;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.*;
+public class Task3 {
+    HashMap<Integer,String> mapName;
+    HashMap<Integer,String> mapSurname;
+    HashMap<Integer,Integer> mapAge;
+    public static void createUsers(){
+        HashMap<Integer,String> mapName = new HashMap<Integer,String>();
+        mapName.put(1,"Иван");
+        mapName.put(2,"Пётр");
+        mapName.put(3,"Пётр");
+        mapName.put(4,"Пётр");
+        mapName.put(5,"Александр");
+        mapName.put(6,"Василий");
+        mapName.put(7,"Иван");
+        mapName.put(8,"Роман");
+        mapName.put(9,"Глеб");
+        mapName.put(10,"Василий");
+        this.mapName=mapName;
+
+
+
+
+    }
+}
