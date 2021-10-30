226a8b213ade877042fe60256691bd0613b7396a
Tanitum <89257070+Tanitum@users.noreply.github.com>
2021-10-30 14:30:08 +0300

    Homework 5
    Homework 5

---
+++ HW5/src/com/company/Note.java
@@ -0,0 +1,48 @@
+package com.company;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class Note
+{
+    public final List<String> notes = new ArrayList<String>();
+    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+
+    public void addNote(int index, String note)
+    {
+        System.out.println("Сейчас будет добавлена заметка [" + note + "] На позицию " + index);
+        lock.writeLock().lock();
+        try {
+            notes.add(index, note);
+        } finally {
+            lock.writeLock().unlock();
+        }
+        System.out.println("Уже добавлена заметка [" + note + "]");
+    }
+
+    public void removeNote(int index)
+    {
+        System.out.println("Сейчас будет удалена заметка с позиции " + index);
+        String note;
+        lock.writeLock().lock();
+        try {
+            note = notes.remove(index);
+        } finally {
+            lock.writeLock().unlock();
+        }
+        System.out.println("Уже удалена заметка [" + note + "] с позиции " + index);
+    }
+
+    public void readNote(int index)
+    {
+        System.out.println("Сейчас будет прочтена заметка с позиции " + index);
+        String note;
+        lock.writeLock().lock();
+        try {
+            note = notes.get(index);
+        } finally {
+            lock.writeLock().unlock();
+        }
+        System.out.println("Прочтена заметка [" + note + "] с позиции " + index);
+    }
+}
